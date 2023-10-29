1975732e491f576d9f620fe4ff1e7d29e3b248ff
bk00119 <briankim00119@gmail.com>
2023-10-28 21:21:09 -0400

    created a user login endpoint and a test with a sample

--- server/endpoints.py
+++ server/endpoints.py
@@ -10,69 +10,21 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-MAIN_MENU = 'MainMenu'
-MAIN_MENU_NM = "Welcome to Text Game!"
-USERS = 'users'
+# Endpoints
+LOGIN_EP = '/login'
+SIGNUP_EP = '/signup'
+
+# Responses
+TOKEN_RESP = 'token'
+USERNAME_RESP = 'username'
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+@api.route(f'{LOGIN_EP}', methods=['POST'])
+class Login(Resource):
     """
-    The purpose of the HelloWorld class is to have a simple test to see if the
-    app is working at all.
+    This class supports fetching a user data for login
     """
-    def get(self):
-        """
-        A trivial endpoint to see if the server is running.
-        It just answers with "hello world."
-        """
-        return {'hello': 'world'}
-
-
-@api.route('/endpoints')
-class Endpoints(Resource):
-    """
-    This class will serve as live, fetchable documentation of what endpoints
-    are available in the system.
-    """
-    def get(self):
-        """
-        The `get()` method will return a list of available endpoints.
-        """
-        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
-        return {"Available endpoints": endpoints}
-
-
-@api.route(f'/{MAIN_MENU}')
-@api.route('/')
-class MainMenu(Resource):
-    """
-    This will deliver our main menu.
-    """
-    def get(self):
-        """
-        Gets the main game menu.
-        """
-        return {'Title': MAIN_MENU_NM,
-                'Default': 2,
-                'Choices': {
-                    '1': {'url': '/', 'method': 'get',
-                          'text': 'List Available Characters'},
-                    '2': {'url': '/',
-                          'method': 'get', 'text': 'List Active Games'},
-                    '3': {'url': f'/{USERS}',
-                          'method': 'get', 'text': 'List Users'},
-                    'X': {'text': 'Exit'},
-                }}
-
-
-@api.route(f'/{USERS}')
-class Users(Resource):
-    """
-    This class supports fetching a list of all pets.
-    """
-    def get(self):
-        """
-        This method returns all users.
-        """
-        return 'Current Users:\nSai\nAbhishek\nKristian\n'
+    def post(self):
+        return {
+            TOKEN_RESP: 'ABC123'
+        }
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -1,6 +1,16 @@
 
 import server.endpoints as ep
 
+TEST_CLIENT = ep.app.test_client()
 
-def test_hello():
-    assert True
+SAMPLE_USER = {
+    'id': 'user1234',
+    'password': 'pw1234'
+}
+
+def test_login():
+    resp = TEST_CLIENT.post(ep.LOGIN_EP, json=SAMPLE_USER)
+    print(f'{resp=}')
+    resp_json = resp.get_json()
+    print(f'{resp_json=}')
+    assert ep.TOKEN_RESP in resp_json
