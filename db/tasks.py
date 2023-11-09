ccbe2348a5b118f7440365e150299cd9e3421f20
Esther Wang <esther.wang@nyu.edu>
2023-11-09 16:12:50 -0500

    created patch for get_task() and made tasks db

---
+++ db/tasks.py
@@ -0,0 +1,21 @@
+"""
+This module interfaces to our tasks ata.
+"""
+
+test_tasks = {
+    "task1": "SWE",
+    "task2": "commits",
+    "task3": "study"
+}
+
+
+def get_tasks():
+    return {
+        "Tasks": ['task1', 'task2', 'task3', 'task4']
+    }
+
+
+def get_test_tasks():
+    return {
+        "Tasks": ['task1', 'task2', 'task3']
+    }
