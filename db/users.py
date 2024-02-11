0dc65909b385f755a4b8b053d630175ddf277900
Esther Wang <esther.wang@nyu.edu>
2024-02-11 16:06:11 -0500

    fixed indent

--- db/users.py
+++ db/users.py
@@ -72,7 +72,8 @@ def retrieve_user(username):
 
 def get_test_user():
     # return {'username': 'user1234', 'password': 'pwpw1234'}
-    return {'first_name': "John", "last_name": "Doe", "username": "johndoe123", "email": "johndoe@gmail.com", "streaks": {'num_streaks': 2, "updated": True}}
+    return {'first_name': "John", "last_name": "Doe", "username": "johndoe123",
+            "email": "johndoe@gmail.com", "streaks": {'num_streaks': 2, "updated": True}}
 
 
 def signup(user):
