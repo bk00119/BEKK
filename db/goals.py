5dd129cc389c6b5d58f47cf84fd97b20cdfc089d
Esther Wang <esther.wang@nyu.edu>
2024-02-19 01:01:44 -0500

    added goals.py db

---
+++ db/goals.py
@@ -0,0 +1,34 @@
+"""
+This module interfaces to our goals data.
+"""
+
+# from pymongo import MongoClient
+import random
+from bson.objectid import ObjectId
+import db.db_connect as dbc
+
+# GOALS COLLECTION:
+# _id: ObjectID
+# user_id: str(ObjectID)
+# is_completed: Bool
+# content: str
+
+GOALS_COLLECT = 'goals'
+ID = '_id'
+USER_ID = 'user_id'
+IS_COMPLETED = 'is_completed'
+CONTENT = 'content'
+
+ID_LEN = 24
+BIG_NUM = 100_000_000_000_000_000_000
+MOCK_ID = MOCK_ID = '0' * ID_LEN
+
+def get_goals():
+    dbc.connect_db()
+    return dbc.fetch_all_as_dict(dbc.DB, GOALS_COLLECT, None)
+
+def get_test_goals():
+    return {
+        "user_id": 'user123', 'is_completed': True,
+        'content': 'complete 20 leetcode problems'
+    }
