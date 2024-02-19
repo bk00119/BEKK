2b81834329d42e5c223ac4164fb391ddda188fee
Esther Wang <esther.wang@nyu.edu>
2024-02-19 01:06:37 -0500

    updated spacing

--- db/goals.py
+++ db/goals.py
@@ -3,8 +3,8 @@ This module interfaces to our goals data.
 """
 
 # from pymongo import MongoClient
-import random
-from bson.objectid import ObjectId
+# import random
+# from bson.objectid import ObjectId
 import db.db_connect as dbc
 
 # GOALS COLLECTION:
@@ -23,9 +23,11 @@ ID_LEN = 24
 BIG_NUM = 100_000_000_000_000_000_000
 MOCK_ID = MOCK_ID = '0' * ID_LEN
 
+
 def get_goals():
     dbc.connect_db()
     return dbc.fetch_all_as_dict(dbc.DB, GOALS_COLLECT, None)
+
 
 def get_test_goals():
     return {
