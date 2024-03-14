4e9725b5dae31bd537ae65e99aafd9ba4d517d06
Kevin Ng <kn2118@nyu.edu>
2024-03-14 00:14:35 -0400

    added user id to test_post

--- db/tests/test_posts.py
+++ db/tests/test_posts.py
@@ -4,18 +4,18 @@ import db.posts as psts
 @pytest.fixture()
 def generate_post_fields():
     return {
-        psts.USER_ID: None, 
+        psts.USER_ID: '65f27756a4817e4be8a2a5e9', 
         psts.IS_COMPLETED: False, 
         psts.CONTENT: "Test Entry", 
         psts.TASK_IDS: [], 
         psts.GOAL_IDS: [], 
         psts.LIKE_IDS: [],
         psts.COMMENT_IDS: [], 
-    }
+    }   
 
 def test_add_post(generate_post_fields):
     """
-    Tests that add_post returned post id 
+    Tests that add_post returned post id
     """
     post_id = psts.add_post(**generate_post_fields)
     assert post_id is not None  
