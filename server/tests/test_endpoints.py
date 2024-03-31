41fbd5feb77d6d82225637fc6800aeac5d2c69ee
Esther Wang <esther.wang@nyu.edu>
2024-03-31 19:58:14 -0400

    implemented add comment endpoint and removed all group related code

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -38,7 +38,7 @@ SAMPLE_USER = {
 SAMPLE_PROFILE = {
     ep.NAME: 'John Adams',
     ep.GOALS: ['cs hw2', 'fin hw3'],
-    ep.GROUPS: ['cs', 'fin'],
+    # ep.GROUPS: ['cs', 'fin'],
     ep.PRIVATE: False
 }
 
@@ -261,39 +261,39 @@ def test_viewUserComments():
         assert isinstance(comments[comment_id], dict)
     cmts.delete_comment(test_comment_id)
 
-@pytest.fixture()
-def setup_viewProfileGroups():
-    usrs.create_user(SAMPLE_USER[ep.USERNAME_RESP], SAMPLE_USER[ep.PASSWORD_RESP])
-    usrs.create_profile(SAMPLE_USER[ep.USERNAME_RESP], SAMPLE_PROFILE[ep.NAME], SAMPLE_PROFILE[ep.GOALS], SAMPLE_PROFILE[ep.GROUPS], SAMPLE_PROFILE[ep.PRIVATE])  
+# @pytest.fixture()
+# def setup_viewProfileGroups():
+#     usrs.create_user(SAMPLE_USER[ep.USERNAME_RESP], SAMPLE_USER[ep.PASSWORD_RESP])
+#     usrs.create_profile(SAMPLE_USER[ep.USERNAME_RESP], SAMPLE_PROFILE[ep.NAME], SAMPLE_PROFILE[ep.GOALS], SAMPLE_PROFILE[ep.GROUPS], SAMPLE_PROFILE[ep.PRIVATE])  
 
 
-def test_viewProfileGroups():
-    resp = TEST_CLIENT.get(ep.VIEWPROFILEGROUPS_EP)
-    resp_json = resp.get_json()
-    assert isinstance(resp_json, dict)
+# def test_viewProfileGroups():
+#     resp = TEST_CLIENT.get(ep.VIEWPROFILEGROUPS_EP)
+#     resp_json = resp.get_json()
+#     assert isinstance(resp_json, dict)
     # assert ep.GROUPS in resp_json
 
 # @pytest.fixture()
 # def setup_deleteGroup():
 #     usrs.delete_group(SAMPLE_USER[ep.USERNAME_RESP], SAMPLE_PROFILE[ep.NAME], SAMPLE_PROFILE[ep.GOALS])
 
-@pytest.mark.skip(reason= "not using this endpoint") 
-def test_deleteGroup():
-    resp = TEST_CLIENT.post(ep.DELETEGROUP_EP, json=SAMPLE_USER)
-    print(f'{resp=}')
-    resp_json = resp.get_json()
-    print(f'{resp_json=}')
-    assert ep.GROUP_RESP in resp_json
-    assert ep.USERNAME_RESP in resp_json
+# @pytest.mark.skip(reason= "not using this endpoint") 
+# def test_deleteGroup():
+#     resp = TEST_CLIENT.post(ep.DELETEGROUP_EP, json=SAMPLE_USER)
+#     print(f'{resp=}')
+#     resp_json = resp.get_json()
+#     print(f'{resp_json=}')
+#     assert ep.GROUP_RESP in resp_json
+#     assert ep.USERNAME_RESP in resp_json
 
 
-@patch('db.profiles.add_group', return_value=pf.MOCK_ID, autospec=True)
-def test_addGroup(mock_add):
-    """
-    Testing for posting a new task successfully: PostTask.post()
-    """
-    resp = TEST_CLIENT.post(ep.ADDGROUP_EP, json=pf.get_new_test_group())
-    assert resp.status_code == OK
+# @patch('db.profiles.add_group', return_value=pf.MOCK_ID, autospec=True)
+# def test_addGroup(mock_add):
+#     """
+#     Testing for posting a new task successfully: PostTask.post()
+#     """
+#     resp = TEST_CLIENT.post(ep.ADDGROUP_EP, json=pf.get_new_test_group())
+#     assert resp.status_code == OK
 
 
 def test_removeProfile():
