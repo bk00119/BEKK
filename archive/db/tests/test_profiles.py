d9bcfa08667bf177310ffa2c1ebd963f9329119c
Esther Wang <esther.wang@nyu.edu>
2024-04-19 12:24:16 -0400

    commented out archive code

--- archive/db/tests/test_profiles.py
+++ archive/db/tests/test_profiles.py
@@ -1,18 +1,18 @@
-import pytest 
+# import pytest 
 
-import db.profiles as pf
+# import db.profiles as pf
 
-def test_test_profile():
-    test_profile = pf.get_test_profile()
-    assert isinstance(test_profile, dict)
+# def test_test_profile():
+#     test_profile = pf.get_test_profile()
+#     assert isinstance(test_profile, dict)
 
-@pytest.mark.skip("working on")
-def test_get_profile():
-    profile = pf.get_profile("656e29138f600af5c067f4de")
-    assert isinstance(profile, dict)
+# @pytest.mark.skip("working on")
+# def test_get_profile():
+#     profile = pf.get_profile("656e29138f600af5c067f4de")
+#     assert isinstance(profile, dict)
 
-def test_add_profile():
-    profile_id = pf.add_profile("john smith", ["cs hw1", "fin hw2"], [], [], True)
-    assert profile_id is not None
+# def test_add_profile():
+#     profile_id = pf.add_profile("john smith", ["cs hw1", "fin hw2"], [], [], True)
+#     assert profile_id is not None
 
      
